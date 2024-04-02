e9a6f0152cafca9b3896e75a9f47a7eb73b2e06b
Planckepoch <Planckepoch@GRIT-CLT-LT-066>
2024-04-02 14:52:17 +0530

    Add project files.

---
+++ CleanMovie.Application/MovieServices.cs
@@ -0,0 +1,33 @@
+﻿using CleanMovie.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanMovie.Application
+{
+    public class MovieServices : IMovieServices
+    {
+        private readonly IMovieRepository imovieRepository;
+
+        public MovieServices (IMovieRepository imovieRepository)
+        {
+            this.imovieRepository = imovieRepository;
+        }
+
+        public Movie CreateMovie(Movie movie)
+        {
+             imovieRepository.CreateMovie(movie);
+
+            return movie;
+        }
+
+        public List<Movie> GetAllMovies()
+        {
+            var movies = imovieRepository.GetAllMovies();
+
+            return movies;
+        }
+    }
+}
